c1a7fd716e1848d8022c814b914b388ff90c27a9
Paul Ryan <paul@sharpred.com>
2013-06-18 17:46:36 +0100

    simple button styling

--- Resources/alloy/controllers/musicPlayerWindow.js
+++ Resources/alloy/controllers/musicPlayerWindow.js
@@ -5,10 +5,61 @@ function Controller() {
     var $ = this;
     var exports = {};
     $.__views.musicwindow = Ti.UI.createWindow({
+        backgroundColor: "white",
+        fullscreen: true,
         id: "musicwindow",
         title: "Music Player"
     });
     $.__views.musicwindow && $.addTopLevelView($.__views.musicwindow);
+    $.__views.__alloyId2 = Ti.UI.createView({
+        backgroundColor: "white",
+        layout: "vertical",
+        height: Ti.UI.SIZE,
+        id: "__alloyId2"
+    });
+    $.__views.musicwindow.add($.__views.__alloyId2);
+    $.__views.startbutton = Ti.UI.createButton({
+        width: 200,
+        height: 50,
+        font: {
+            fontSize: 18
+        },
+        top: 25,
+        borderRadius: 16,
+        borderWidth: 1,
+        color: "black",
+        title: "Start",
+        id: "startbutton"
+    });
+    $.__views.__alloyId2.add($.__views.startbutton);
+    $.__views.pausebutton = Ti.UI.createButton({
+        width: 200,
+        height: 50,
+        font: {
+            fontSize: 18
+        },
+        top: 25,
+        borderRadius: 16,
+        borderWidth: 1,
+        color: "black",
+        title: "Pause",
+        id: "pausebutton"
+    });
+    $.__views.__alloyId2.add($.__views.pausebutton);
+    $.__views.stopbutton = Ti.UI.createButton({
+        width: 200,
+        height: 50,
+        font: {
+            fontSize: 18
+        },
+        top: 25,
+        borderRadius: 16,
+        borderWidth: 1,
+        color: "black",
+        title: "Stop",
+        id: "stopbutton"
+    });
+    $.__views.__alloyId2.add($.__views.stopbutton);
     exports.destroy = function() {};
     _.extend($, $.__views);
     exports.openMainWindow = function(_tab) {
--- Resources/alloy/controllers/tabOneView.js
+++ Resources/alloy/controllers/tabOneView.js
@@ -11,13 +11,13 @@ function Controller() {
         id: "main",
         title: "POD Catcher"
     });
-    $.__views.__alloyId2 = Ti.UI.createView({
+    $.__views.__alloyId3 = Ti.UI.createView({
         backgroundColor: "white",
         layout: "vertical",
         height: Ti.UI.SIZE,
-        id: "__alloyId2"
+        id: "__alloyId3"
     });
-    $.__views.main.add($.__views.__alloyId2);
+    $.__views.main.add($.__views.__alloyId3);
     $.__views.syncbutton = Ti.UI.createButton({
         width: 200,
         height: 50,
@@ -31,7 +31,7 @@ function Controller() {
         title: "Sync",
         id: "syncbutton"
     });
-    $.__views.__alloyId2.add($.__views.syncbutton);
+    $.__views.__alloyId3.add($.__views.syncbutton);
     $.__views.basicSwitch = Ti.UI.createSwitch({
         titleOn: "Offline: ON",
         titleOff: "Offline: OFF",
@@ -47,7 +47,7 @@ function Controller() {
         color: "black",
         id: "basicSwitch"
     });
-    $.__views.__alloyId2.add($.__views.basicSwitch);
+    $.__views.__alloyId3.add($.__views.basicSwitch);
     outputState ? $.__views.basicSwitch.addEventListener("change", outputState) : __defers["$.__views.basicSwitch!change!outputState"] = true;
     $.__views.taboneview = Ti.UI.createTab({
         font: {
